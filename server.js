ab4717d5270f831b05db7faf920ec2d9c6c941b7
Jaryd Knight <jaryd.knight@hotmail.com>
2022-07-17 16:48:37 -0400

    complete html routes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes/index");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/", htmlRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`app now listening on port ${PORT}`)
