f37306786a7dd39b45cbc758523bd0f7ea9e3a40
Jaryd Knight <jaryd.knight@hotmail.com>
2022-07-17 20:41:33 -0400

    comment code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,6 +5,7 @@ const validateNote = require("../../lib/validateNote");
 const createNewNote = require("../../lib/createNewNote");
 const saveToDb = require("../../lib/saveToDb")
 
+// Route to get notes from db.json
 router.get("/notes", (req, res) => {
     let data = fs.readFileSync(path.join(__dirname, "../../db/db.json"), err => {
         if (err) {
@@ -15,6 +16,7 @@ router.get("/notes", (req, res) => {
     res.json(JSON.parse(data));
 });
 
+// Route to save new notes to db.json
 router.post("/notes", (req, res) => {
     if(!validateNote(req.body)) {
         res.status(400).send("The note is not properly formatted")
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const path = require("path");
 
+// Route to get to index.html
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
+// Route to get to notes.html
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
 
+// Route to handle any other input. Will serve index.html to client
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
